f70a2607a577696bd3800913b6c5e1441884b334
RdkDvv <rdkdvv@gmail.com>
2018-04-25 19:47:07 +0300

    Create a first template of game

---
+++ NumberWizardUI/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+    
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Game over!!! GoodBye!");
+    }
+
+}
