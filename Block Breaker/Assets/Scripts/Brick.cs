7202d80e2c578393deaa318d632de6ed46bcb714
RdkDvv <rdkdvv@gmail.com>
2018-05-04 15:26:38 +0300

    add bricks for scene

---
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+
+    public int maxHits = 5;
+
+    private int timeHit;
+
+    // Use this for initialization
+    void Start ()
+    {
+        timeHit = 0;
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        timeHit++;
+    }
+}
