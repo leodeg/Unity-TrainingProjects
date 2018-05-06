75f32deec81576f42078532f5a1b90ad944fdd13
RdkDvv <rdkdvv@gmail.com>
2018-05-06 16:08:21 +0300

    Add destruction effect for bricks

--- Block Breaker/Assets/Scripts/Brick.cs
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -5,32 +5,43 @@ using UnityEngine;
 public class Brick : MonoBehaviour
 {
 
-    public int maxHits = 6;
+    public Sprite[] hitSprites;
 
-    private int timeHit;
-
+    private int timesHit;
     private LevelManager levelManager;
 
-    // Use this for initialization
     void Start ()
     {
-        timeHit = 0;
+        timesHit = 0;
         levelManager = GameObject.FindObjectOfType<LevelManager>();
     }
     
-    // Update is called once per frame
     void Update ()
     {
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        timeHit++;
+        bool isBreakable = (this.tag == "Breakable");
+        if (isBreakable)
+            HandleHits();
+    }
 
-        if (timeHit >= maxHits)
-        {
+    void HandleHits()
+    {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
             Destroy(gameObject);
-        }
+        else
+            LoadSprites();
+    }
+
+    void LoadSprites()
+    {
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex])
+            this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
     }
 }
