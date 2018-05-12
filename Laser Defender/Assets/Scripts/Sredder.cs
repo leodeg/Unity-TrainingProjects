31001eb723edf6d3708b382c3f8c90f50255f688
RdkDvv <rdkdvv@gmail.com>
2018-05-12 22:09:18 +0300

    Add shredder

--- Laser Defender/Assets/Scripts/Sredder.cs
+++ Laser Defender/Assets/Scripts/Sredder.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sredder : MonoBehaviour {
+public class Sredder : MonoBehaviour
+{
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Destroy the object (player laser in our case) which is in the collider
         Destroy(collision.gameObject);
     }
 }
