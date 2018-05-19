c38104a495c125885548f3875f375998794ce6d8
RdkDvv <rdkdvv@gmail.com>
2018-05-19 20:05:43 +0300

    Fix of game resolutions

---
+++ Glitch Garden A Plants vs Zombies Clone/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Want to Quit!!!");
+        Application.Quit();  
+    }
+
+    public void LoadNextLevel()
+    {
+        //The old way of load next scene
+        //Application.LoadLevel(Application.loadedLevel + 1);
+
+        //Load the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
