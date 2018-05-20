515bc2bfbc5bb9b13a4e2979f82a6b0a56fb31dc
RdkDvv <rdkdvv@gmail.com>
2018-05-20 16:24:27 +0300

    Add hierarchy of scenes

--- Glitch Garden A Plants vs Zombies Clone/Assets/Scripts/LevelManager.cs
+++ Glitch Garden A Plants vs Zombies Clone/Assets/Scripts/LevelManager.cs
@@ -5,25 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public float autoLoadNextLevelAfter;
+
+    private void Start()
+    {
+        if (autoLoadNextLevelAfter <= 0)
+        {
+            Debug.Log("Level auto load disabled, use a positive number is seconds");
+        }
+        else
+        {
+            Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        }
+    }
 
     public void LoadLevel(string name)
     {
-        Debug.Log("Level load requested for: " + name);
+        Debug.Log("New Level load: " + name);
         SceneManager.LoadScene(name);
     }
 
     public void QuitRequest()
     {
-        Debug.Log("Want to Quit!!!");
+        Debug.Log("Quit requested");
         Application.Quit();  
     }
 
     public void LoadNextLevel()
     {
-        //The old way of load next scene
-        //Application.LoadLevel(Application.loadedLevel + 1);
-
-        //Load the next scene
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Application.LoadLevel(Application.loadedLevel + 1);
     }
 }
