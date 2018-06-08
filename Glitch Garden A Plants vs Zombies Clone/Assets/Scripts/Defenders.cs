c2acbcae0ee6fabfa3dd24a9e6ee57148a9bd5a3
RdkDvv <rdkdvv@gmail.com>
2018-06-08 20:34:56 +0300

    Add firing projectiles

--- Glitch Garden A Plants vs Zombies Clone/Assets/Scripts/Defenders.cs
+++ Glitch Garden A Plants vs Zombies Clone/Assets/Scripts/Defenders.cs
@@ -4,21 +4,5 @@ using UnityEngine;
 
 public class Defenders : MonoBehaviour
 {
-
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log(name + " trigger enter");
-    }
+    // Used as a tag
 }
