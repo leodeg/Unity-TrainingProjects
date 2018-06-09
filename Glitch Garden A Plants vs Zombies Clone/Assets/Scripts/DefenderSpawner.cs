847788c406c37be2e185dc72457eb911a7b28163
RdkDvv <rdkdvv@gmail.com>
2018-06-09 15:38:09 +0300

    Add grid for spawner of defenders

---
+++ Glitch Garden A Plants vs Zombies Clone/Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    public Camera myCamera;
+
+    private GameObject defenderParent;
+
+    // Use this for initialization
+    void Start ()
+    {
+        defenderParent = GameObject.Find("Defenders");
+
+        if (!defenderParent)
+        {
+            defenderParent = new GameObject("Defenders");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        Vector2 rawPosition = CalculateWordlPointOfMouseClick();
+
+        GameObject newDefender = Instantiate(Button.selectedDefender, SnapToGrid(rawPosition), Quaternion.identity) as GameObject;
+
+        newDefender.transform.parent = defenderParent.transform;
+    }
+
+    Vector2 SnapToGrid(Vector2 rawWorldPosition)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPosition.x);
+        float newY = Mathf.RoundToInt(rawWorldPosition.y);
+        return new Vector2(newX, newY);
+    }
+
+    Vector2 CalculateWordlPointOfMouseClick()
+    {
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+        float distanceFromCamera = 10f;
+
+        Vector3 weirdTriplet = new Vector3(mouseX, mouseY, distanceFromCamera);
+        Vector2 worldPosition = myCamera.ScreenToWorldPoint(weirdTriplet);
+
+        return worldPosition;
+    }
+}
