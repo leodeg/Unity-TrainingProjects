1fa7fc61ccb7357b9643e5c5803b46a33dcbe770
RdkDvv <rdkdvv@gmail.com>
2018-06-09 10:37:56 +0300

    Add damage from projectiles

--- Glitch Garden A Plants vs Zombies Clone/Assets/Scripts/Projectile.cs
+++ Glitch Garden A Plants vs Zombies Clone/Assets/Scripts/Projectile.cs
@@ -7,6 +7,8 @@ public class Projectile : MonoBehaviour
 
     public float speed, damage;
 
+    private GameObject currentTarget;
+
     // Use this for initialization
     void Start ()
     {
@@ -17,5 +19,18 @@ public class Projectile : MonoBehaviour
     void Update ()
     {
         transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Attackers attackers = collider.GetComponent<Attackers>();
+        Health health = collider.GetComponent<Health>();
+
+        if (attackers && health)
+        {
+            health.DealDamage(damage);
+            // destroy projectile
+            Destroy(gameObject);
+        }
     }
 }
