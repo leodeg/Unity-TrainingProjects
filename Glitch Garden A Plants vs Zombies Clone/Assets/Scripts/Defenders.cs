948f16c8d38d033f6e7e4133f5e6cced99c9bb4e
RdkDvv <rdkdvv@gmail.com>
2018-06-13 16:36:10 +0300

    Add score of stars

--- Glitch Garden A Plants vs Zombies Clone/Assets/Scripts/Defenders.cs
+++ Glitch Garden A Plants vs Zombies Clone/Assets/Scripts/Defenders.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class Defenders : MonoBehaviour
 {
+    private StarDisplay starDisplay;
+
+    void Start()
+    {
+        starDisplay = GameObject.FindObjectOfType<StarDisplay>();
+    }
     // Used as a tag
+
+    public void AddStars(int amount)
+    {
+        starDisplay.AddStars(amount);
+    }
+
 }
---
+++ Glitch Garden A Plants vs Zombies Clone/Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(Text))]
+public class StarDisplay : MonoBehaviour
+{
+    private Text text;
+    private int stars;
+
+	// Use this for initialization
+	void Start ()
+    {
+        text = GetComponent<Text>();
+	}
+
+    public void AddStars(int amount)
+    {
+        stars += amount;
+        UpdateDisplay();
+    }
+
+    public void UseStars(int amount)
+    {
+        stars -= amount;
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        text.text = stars.ToString();
+    }
+}
