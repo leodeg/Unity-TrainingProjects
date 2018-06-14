cb56fa5779e4676239968c7f67c355d6da45eb67
RdkDvv <rdkdvv@gmail.com>
2018-06-14 20:16:07 +0300

    Add level timer

---
+++ Glitch Garden A Plants vs Zombies Clone/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    public float levelSeconds = 5;
+
+    private Slider slider;
+    private AudioSource audioSource;
+    private bool isEndOfLevel = false;
+    private LevelManager levelManager;
+
+	void Start ()
+    {
+        slider = GetComponent<Slider>();
+        audioSource = GetComponent<AudioSource>();
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	void Update ()
+    {
+        slider.value = 1 - (Time.timeSinceLevelLoad / levelSeconds);
+        
+        if (Time.timeSinceLevelLoad >= levelSeconds && !isEndOfLevel)
+        {
+            audioSource.Play();
+            Invoke("LoadNextLevel", audioSource.clip.length);
+            isEndOfLevel = true;
+        }
+	}
+
+    void LoadNextLevel()
+    {
+        levelManager.LoadNextLevel();
+    }
+}
---
+++ Glitch Garden A Plants vs Zombies Clone/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour
+{
+    private LevelManager levelManager;
+
+	void Start ()
+    {
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        levelManager.LoadLevel("03 Lose");
+    }
+}
